e7cea96040fc152d10e92b311e13fede90240300
Eduardo Patrocinio <epatro@gmail.com>
2017-07-27 18:17:17 -0400

    Build ICp image

--- build_icp.sh
+++ build_icp.sh
@@ -1,5 +1,9 @@
 vagrant init ubuntu/xenial64
 vagrant up
 
+
 # Install ICP
 vagrant ssh -c "/vagrant/vagrant/install_icp.sh"
+
+# Pack ICP box
+vagrant package --output icp-image
--- vagrant/install_icp.sh
+++ vagrant/install_icp.sh
@@ -1,6 +1,6 @@
 echo Installing Docker
 sudo apt-get update
-sudo apt-get upgrade -y
+sudo apt-get upgrade -ytocuh 
 sudo apt-get install -y linux-image-extra-virtual
 sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
@@ -8,3 +8,12 @@ sudo apt-get -y install python-software-properties
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb\_release -cs) stable"
 sudo apt-get update
 sudo apt-get install -y docker-ce
+
+echo Validating Docker install
+sudo docker run hello-world
+
+echo Install docker.py
+sudo apt-get install -y python-setuptools
+sudo easy_install pip
+sudo -H pip install docker-py>=1.7.0
+
